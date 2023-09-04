29bb04c42a0a95b618b1aee722ae4e4150156ec6
Erick Freitas <erick.freitas@trin.ca>
2023-09-04 20:53:36 -0300

    first commit

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@erickzic/design-system";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl">Web</h1>
+      <Button>Boop</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ apps/workshop/postcss.config.js
@@ -0,0 +1,1 @@
+module.exports = require("@erickzic/design-system/postcss");
